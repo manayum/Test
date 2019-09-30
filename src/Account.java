7f6e6eb1d3e96af682b599ef0b63c469dcaa9c8a
khadijah <khadijah@ST-W10H>
2019-09-29 23:22:02 -0400

    Added close() method in Account class.

--- src/Account.java
+++ src/Account.java
@@ -3,9 +3,11 @@ public class Account {
 	
 	public void open(String account) {
 		
-		System.out.println(account + " opened. ");
+		System.out.println(account + " opened. ");	
 		
-		
+	}
+	public void close() {
+		System.out.println("Account is closed.");
 	}
 
 }
